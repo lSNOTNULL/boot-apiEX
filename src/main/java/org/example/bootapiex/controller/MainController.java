aa395bd930c8c7998e70a6fb44320dd9c1342362
lSNOTNULL <iream1@naver.com>
2025-04-16 16:18:22 +0900

    List 결과 스타일

--- src/main/java/org/example/bootapiex/controller/MainController.java
+++ src/main/java/org/example/bootapiex/controller/MainController.java
@@ -1,9 +1,7 @@
 package org.example.bootapiex.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.bootapiex.model.entity.Diary;
-import org.example.bootapiex.model.form.DiaryForm;
-import org.example.bootapiex.service.DiaryService;
+
 import org.example.bootapiex.service.StorageService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,35 +11,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
     private final StorageService storageService;
-    private final DiaryService diaryService;
+
 
     @GetMapping
-    public String index(Model model) throws Exception {
-        model.addAttribute("message", "후참잘");
-        model.addAttribute("title", "프레리독");
-        model.addAttribute("form", DiaryForm.empty());
-        model.addAttribute("list", diaryService.getAllDiaryList());
+    public String index(Model model) {
+        model.addAttribute("title", "✨ AI 일기 앱");
+        model.addAttribute("message", "AI가 당신의 일기의 숨은 뜻을 해석해드립니다!");
+
+
         return "index";
     }
-    @PostMapping
-    public String post(DiaryForm form, RedirectAttributes redirectAttributes) throws Exception {
-        String imageName = storageService.upload(form.file());
-        redirectAttributes.addFlashAttribute("image",imageName);
-        Diary diary = new Diary();
-        diary.setTitle(form.title());
-        diary.setContent(form.content());
-        diary.setFilename(imageName);
-        diaryService.createDiary(diary);
 
-        return "redirect:/";
-    }
     @GetMapping("/file/{filename}")
     public ResponseEntity<byte[]> file(@PathVariable String filename) {
         try {
